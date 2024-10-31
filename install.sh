722d3008f4c80cc4bb26027c5008162b224affb5
Wojciech Knapik <wmknapik@gmail.com>
2024-10-31 23:48:41 +0100

    wip

--- install.sh
+++ install.sh
@@ -10,114 +10,95 @@ set -eu
 # Helpers
 error() { exec >&2; printf "Error: "; printf "%s\n" "${@:?}"; exit 1; }
 is_command() { command -v "${1:?}" >/dev/null; }
+show() { (set -x; "$@"); }
 
 # All the code is wrapped in a main function that gets called at the
 # bottom of the file, so that a truncated partial download doesn't end
 # up executing half a script.
 main() {
-    ARCH="$(uname -m)"
-    GLIBC_VER="$(ldd --version 2>/dev/null|head -n1|grep -oE '[0-9]+\.[0-9]+$' || true)"
-    GLIBC_VER_MIN="2.26"
-
     ## Check if the browser can run on this system
 
-    if [ "$(printf "%s\n%s" "$GLIBC_VER" "$GLIBC_VER_MIN"|sort -V|head -n1)" != "$GLIBC_VER_MIN" ]; then
-        error "Unsupported glibc version ${GLIBC_VER:-<empty>}. Only glibc versions >=$GLIBC_VER_MIN are supported."
+    arch="$(uname -m)"
+    glibc_ver="$(ldd --version 2>/dev/null|head -n1|grep -oE '[0-9]+\.[0-9]+$' || true)"
+    glibc_ver_min="2.26"
+
+    if [ "$(printf "%s\n%s" "$glibc_ver" "$glibc_ver_min"|sort -V|head -n1)" != "$glibc_ver_min" ]; then
+        error "Unsupported glibc version ${glibc_ver:-<empty>}. Only glibc versions >=$glibc_ver_min are supported."
     fi
 
-    case "$ARCH" in
+    case "$arch" in
         aarch64|arm64|x86_64) ;;
-        *) error "Unsupported architecture $ARCH. Only 64-bit x86 or ARM machines are supported.";;
+        *) error "Unsupported architecture $arch. Only 64-bit x86 or ARM machines are supported.";;
     esac
 
     ## Find and/or install necessary tools
 
     if [ "$(id -u)" = 0 ]; then
-        SUDO=""
+        sudo=""
     elif is_command sudo; then
-        SUDO="sudo"
+        sudo="sudo"
     elif is_command doas; then
-        SUDO="doas"
+        sudo="doas"
     else
         error "Please install sudo or doas (or run this script as root) to proceed."
     fi
 
     if is_command curl; then
-        CURL="curl -fsS"
+        curl="curl -fsS"
     elif is_command wget; then
-        CURL="wget -qO-"
+        curl="wget -qO-"
     elif is_command apt-get; then
-        CURL="curl -fsS"
+        curl="curl -fsS"
         export DEBIAN_FRONTEND=noninteractive
-        set -x
-        $SUDO apt-get update
-        $SUDO apt-get install -y curl
-        set +x
+        show $sudo apt-get update
+        show $sudo apt-get install -y curl
     fi
 
     ## Install the browser
 
     if is_command apt-get; then
         export DEBIAN_FRONTEND=noninteractive
-        set -x
-        $SUDO mkdir -p --mode=0755 /usr/share/keyrings
-        $CURL "https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"|\
-            $SUDO tee /usr/share/keyrings/brave-browser-archive-keyring.gpg >/dev/null
-        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|\
-            $SUDO tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null
-        $SUDO apt-get update
-        $SUDO apt-get install -y brave-browser
-        set +x
+        show $sudo mkdir -p --mode=0755 /usr/share/keyrings
+        show $curl "https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"|\
+            show $sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg >/dev/null
+        show echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|\
+            show $sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null
+        show $sudo apt-get update
+        show $sudo apt-get install -y brave-browser
     elif is_command dnf; then
-        set -x
-        $SUDO dnf install -y 'dnf-command(config-manager)'
-        $SUDO dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-        $SUDO rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        $SUDO dnf install -y brave-browser
-        set +x
+        show $sudo dnf install -y 'dnf-command(config-manager)'
+        show $sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+        show $sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+        show $sudo dnf install -y brave-browser
     elif is_command yum; then
         if ! is_command yum-config-manager; then
-            set -x
-            $SUDO yum install yum-utils -y
-            set +x
+            show $sudo yum install yum-utils -y
         fi
-        set -x
-        $SUDO yum-config-manager -y --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-        $SUDO rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        $SUDO yum install brave-browser -y
-        set +x
+        show $sudo yum-config-manager -y --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+        show $sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+        show $sudo yum install brave-browser -y
     elif is_command zypper; then
-        set -x
-        $SUDO rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-        $SUDO zypper --non-interactive ar -g -r https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-        $SUDO zypper --non-interactive --gpg-auto-import-keys refresh
-        $SUDO zypper --non-interactive install brave-browser
-        set +x
+        show $sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+        show $sudo zypper --non-interactive ar -g -r https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+        show $sudo zypper --non-interactive --gpg-auto-import-keys refresh
+        show $sudo zypper --non-interactive install brave-browser
     elif is_command pacman; then
+        pacman_opts="-Sy --needed --noconfirm"
         if pacman -Ss brave-browser >/dev/null 2>&1; then
-            set -x
-            $SUDO pacman -Sy --needed --noconfirm brave-browser
-            set +x
+            show $sudo pacman $pacman_opts brave-browser
         elif is_command paru; then
-            AUR_HELPER="paru"
+            show paru $pacman_opts brave-bin
         elif is_command pikaur; then
-            AUR_HELPER="pikaur"
+            show pikaur $pacman_opts brave-bin
         elif is_command yay; then
-            AUR_HELPER="yay"
+            show yay $pacman_opts brave-bin
         else
             error "Could not find an AUR helper. Please install paru, pikaur, or yay to proceed." "" \
                 "You can find more information about AUR helpers at https://wiki.archlinux.org/title/AUR_helpers"
         fi
-        if [ "${AUR_HELPER:-}" ]; then
-            set -x
-            "$AUR_HELPER" -Sy --needed --noconfirm brave-bin
-            set +x
-        fi
     elif [ "$(uname)" = Darwin ]; then
         if is_command brew; then
-            set -x
-            NONINTERACTIVE=1 brew install --cask brave-browser
-            set +x
+            NONINTERACTIVE=1 show brew install --cask brave-browser
         else
             error "Could not find brew. Please install brew to proceed." ""\
                 "A Brave .dmg can also be downloaded from https://brave.com/download/"
@@ -130,7 +111,7 @@ main() {
             "$(cat /etc/os-release)"
     fi
 
-    echo "Installation complete! Start Brave by typing $([ "${AUR_HELPER:-}" ] && echo brave || echo brave-browser)."
+    echo "Installation complete! Start Brave by typing $(command -v brave-browser || command -v brave)."
 }
 
 main
