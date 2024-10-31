796b914abba8cf720487a1ab86d05b45966a837a
Wojciech Knapik <wmknapik@gmail.com>
2024-11-01 00:03:13 +0100

    wip

--- install.sh
+++ install.sh
@@ -55,11 +55,11 @@ main() {
     fi
 
     ## Install the browser
-    # shellcheck disable=SC2086 # due to show()
 
     if is_command apt-get; then
         export DEBIAN_FRONTEND=noninteractive
         show $sudo mkdir -p --mode=0755 /usr/share/keyrings
+        # shellcheck disable=SC2086 # due to show + $sudo
         show $curl "https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"|\
             show $sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg >/dev/null
         show echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|\
@@ -85,6 +85,7 @@ main() {
         show $sudo zypper --non-interactive install brave-browser
     elif is_command pacman; then
         pacman_opts="-Sy --needed --noconfirm"
+        # shellcheck disable=SC2086 # due to show + $pacman_opts
         if pacman -Ss brave-browser >/dev/null 2>&1; then
             show $sudo pacman $pacman_opts brave-browser
         elif is_command paru; then
